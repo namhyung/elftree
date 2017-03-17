67efe0cae523799cd6a989fc18f411b419199aed
Namhyung Kim <namhyung@gmail.com>
2017-03-18 00:49:00 +0900

    Implement expand/fold using <ENTER> key in TUI
    Display entry with +/- sign and fold it dynamically in TUI
    
      $ elftree -tui /usr/bin/ls
      - ls
         - libcap.so.2                   <-- cursor
            - libc.so.6
               - ld-linux-x86-64.so.2
         - libc.so.6
    
    When pressing <ENTER> on libcap.so.2, the output will be
    
      - ls
         + libcap.so.2
         - libc.so.6
    
    This will help to navigate a binary with long list of dependencies (like
    an GUI application).

--- tui.go
+++ tui.go
@@ -2,16 +2,27 @@ package main
 
 import tui "github.com/gizak/termui"
 
+type TreeItem struct {
+	n       *DepsNode
+	parent  *TreeItem
+	sibling *TreeItem
+	child   *TreeItem // pointer to first child
+	folded  bool
+	total   int // number of (shown) children (not count itself)
+}
+
 type ScrollList struct {
-	tui.Block    // embedded
-	Items        []string
+	tui.Block // embedded
+	Items     *TreeItem
+	Curr      *TreeItem
+
 	ItemFgColor  tui.Attribute
 	ItemBgColor  tui.Attribute
 	FocusFgColor tui.Attribute
 	FocusBgColor tui.Attribute
 
-	idx int
-	off int
+	idx int // current cursor position
+	off int // first entry displayed
 }
 
 func NewScrollList() *ScrollList {
@@ -26,14 +37,68 @@ func NewScrollList() *ScrollList {
 	return l
 }
 
+func (i *TreeItem) next() *TreeItem {
+	if i.child == nil || i.folded {
+		for i != nil {
+			if i.sibling != nil {
+				return i.sibling
+			}
+
+			i = i.parent
+		}
+		return nil
+	}
+	return i.child
+}
+
+func (i *TreeItem) expand() {
+	if !i.folded || i.child == nil {
+		return
+	}
+
+	for c := i.child; c != nil; c = c.sibling {
+		i.total += c.total + 1
+	}
+
+	for p := i.parent; p != nil; p = p.parent {
+		p.total += i.total
+	}
+
+	i.folded = false
+}
+
+func (i *TreeItem) fold() {
+	if i.folded || i.child == nil {
+		return
+	}
+
+	for p := i.parent; p != nil; p = p.parent {
+		p.total -= i.total
+	}
+	i.total = 0
+
+	i.folded = true
+}
+
+func (i *TreeItem) toggle() {
+	if i.folded {
+		i.expand()
+	} else {
+		i.fold()
+	}
+}
+
 // Buffer implements Bufferer interface.
 func (l *ScrollList) Buffer() tui.Buffer {
 	buf := l.Block.Buffer()
 
+	i := 0
 	printed := 0
 
-	for i, v := range l.Items {
+	var ti *TreeItem
+	for ti = l.Items; ti != nil; ti = ti.next() {
 		if i < l.off {
+			i++
 			continue
 		}
 		if printed == l.Height-2 {
@@ -45,12 +110,32 @@ func (l *ScrollList) Buffer() tui.Buffer {
 		if i == l.idx {
 			fg = l.FocusFgColor
 			bg = l.FocusBgColor
+
+			l.Curr = ti
 		}
 
-		cs := tui.DefaultTxBuilder.Build(v, fg, bg)
-		cs = tui.DTrimTxCls(cs, l.Width-2)
+		cs := tui.DefaultTxBuilder.Build(ti.n.name, fg, bg)
+		cs = tui.DTrimTxCls(cs, l.Width-2-2-3*ti.n.depth)
 
 		j := 0
+		if i == l.idx {
+			// draw current line cursor from the beginning
+			for j < 3*ti.n.depth {
+				buf.Set(j+1, printed+1, tui.Cell{' ', fg, bg})
+				j++
+			}
+		} else {
+			j = 3 * ti.n.depth
+		}
+
+		if ti.folded {
+			buf.Set(j+1, printed+1, tui.Cell{'+', fg, bg})
+		} else {
+			buf.Set(j+1, printed+1, tui.Cell{'-', fg, bg})
+		}
+		buf.Set(j+2, printed+1, tui.Cell{' ', fg, bg})
+		j += 2
+
 		for _, vv := range cs {
 			w := vv.Width()
 			buf.Set(j+1, printed+1, vv)
@@ -58,8 +143,9 @@ func (l *ScrollList) Buffer() tui.Buffer {
 		}
 
 		printed++
+		i++
 
-		if i != l.idx {
+		if i != l.idx+1 {
 			continue
 		}
 
@@ -73,7 +159,7 @@ func (l *ScrollList) Buffer() tui.Buffer {
 }
 
 func (l *ScrollList) Down() {
-	if l.idx < len(l.Items)-1 {
+	if l.idx < l.Items.total {
 		l.idx++
 	}
 	if l.idx-l.off >= l.Height-2 {
@@ -90,22 +176,28 @@ func (l *ScrollList) Up() {
 	}
 }
 
-func makeItems(dep *DepsNode, items []string) []string {
-	str := ""
-	for i := 0; i < dep.depth; i++ {
-		str += "   "
-	}
-	str += dep.name
+func (l *ScrollList) Toggle() {
+	l.Curr.toggle()
+}
 
-	if showPath {
-		str += " => " + deps[dep.name].path
-	}
+func makeItems(dep *DepsNode, parent *TreeItem) *TreeItem {
+	item := &TreeItem{n: dep, parent: parent, folded: false, total: len(dep.child)}
 
-	items = append(items, str)
+	var prev *TreeItem
 	for _, v := range dep.child {
-		items = makeItems(v, items)
+		c := makeItems(v, item)
+
+		if item.child == nil {
+			item.child = c
+		}
+		if prev != nil {
+			prev.sibling = c
+		}
+		prev = c
+
+		item.total += c.total
 	}
-	return items
+	return item
 }
 
 func ShowWithTUI(dep *DepsNode) {
@@ -114,7 +206,7 @@ func ShowWithTUI(dep *DepsNode) {
 	}
 	defer tui.Close()
 
-	items := makeItems(dep, []string{})
+	items := makeItems(dep, nil)
 
 	ls := NewScrollList()
 
@@ -122,6 +214,7 @@ func ShowWithTUI(dep *DepsNode) {
 	ls.Height = tui.TermHeight()
 	ls.Width = tui.TermWidth()
 	ls.Items = items
+	ls.Curr = items
 
 	tui.Render(ls)
 
@@ -141,6 +234,11 @@ func ShowWithTUI(dep *DepsNode) {
 	})
 	tui.Handle("/sys/kbd/<up>", func(tui.Event) {
 		ls.Up()
+		tui.Render(ls)
+	})
+
+	tui.Handle("/sys/kbd/<enter>", func(tui.Event) {
+		ls.Toggle()
 		tui.Render(ls)
 	})
 
