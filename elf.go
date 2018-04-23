f51f908f61b2e114dca95ec446fb2578f27fc024
Namhyung Kim <namhyung@gmail.com>
2018-04-23 16:29:20 +0900

    move ELF related code to elf.go

---
+++ elf.go
@@ -0,0 +1,214 @@
+/*
+ * ELF tree - Tree viewer for ELF library dependency
+ *
+ * Copyright (C) 2017-2018  Namhyung Kim <namhyung@gmail.com>
+ *
+ * Released under MIT license.
+ */
+package main
+
+import (
+	"debug/elf"
+	"fmt"
+	str "strings"
+)
+
+const (
+	GNU_EH_FRAME = elf.PT_LOOS + 74769744
+	GNU_STACK    = elf.PT_LOOS + 74769745
+	GNU_RELRO    = elf.PT_LOOS + 74769746
+)
+
+func progHdrString(phdr *elf.Prog) string {
+	var typeStr string
+	var flagStr string
+
+	switch phdr.Type {
+	case GNU_EH_FRAME:
+		typeStr = "GNU_EH_FRAME"
+	case GNU_STACK:
+		typeStr = "GNU_STACK"
+	case GNU_RELRO:
+		typeStr = "GNU_RELRO"
+	default:
+		typeStr = phdr.Type.String()[3:]
+	}
+
+	switch phdr.Flags {
+	case elf.PF_X:
+		flagStr = "__X"
+	case elf.PF_W:
+		flagStr = "_W_"
+	case elf.PF_R:
+		flagStr = "R__"
+	case elf.PF_R | elf.PF_W:
+		flagStr = "RW_"
+	case elf.PF_R | elf.PF_X:
+		flagStr = "R_X"
+	case elf.PF_R | elf.PF_W | elf.PF_X:
+		flagStr = "RWX"
+	default:
+		flagStr = "???"
+	}
+
+	return fmt.Sprintf("%-16s  %s    %#8x  %#8x  %#8x", typeStr, flagStr, phdr.Vaddr, phdr.Memsz, phdr.Align)
+}
+
+const (
+	DT_GNU_HASH   = elf.DT_HIOS + 3829
+	DT_RELACOUNT  = elf.DT_VERSYM + 9
+	DT_RELCOUNT   = elf.DT_VERSYM + 10
+	DT_FLAGS_1    = elf.DT_VERSYM + 11
+	DT_VERDEF     = elf.DT_VERSYM + 12
+	DT_VERDEFNUM  = elf.DT_VERSYM + 13
+	DT_VERNEED    = elf.DT_VERSYM + 14
+	DT_VERNEEDNUM = elf.DT_VERSYM + 15
+)
+
+// convert DT_FLAGS
+func strFlags(val uint64) string {
+	var ret []string
+
+	if (val & 0x1) != 0 {
+		ret = append(ret, "ORIGIN")
+	}
+	if (val & 0x2) != 0 {
+		ret = append(ret, "SYMBOLIC")
+	}
+	if (val & 0x4) != 0 {
+		ret = append(ret, "TEXTREL")
+	}
+	if (val & 0x8) != 0 {
+		ret = append(ret, "BIND_NOW")
+	}
+	if (val & 0x10) != 0 {
+		ret = append(ret, "STATIC_TLS")
+	}
+
+	return str.Join(ret, "|")
+}
+
+// convert DT_FLAGS_1
+func strFlags1(val uint64) string {
+	var ret []string
+
+	if (val & 0x1) != 0 {
+		ret = append(ret, "NOW")
+	}
+	if (val & 0x2) != 0 {
+		ret = append(ret, "GLOBAL")
+	}
+	if (val & 0x4) != 0 {
+		ret = append(ret, "GROUP")
+	}
+	if (val & 0x8) != 0 {
+		ret = append(ret, "NODELETE")
+	}
+	if (val & 0x10) != 0 {
+		ret = append(ret, "LOADFLTR")
+	}
+	if (val & 0x20) != 0 {
+		ret = append(ret, "INITFIRST")
+	}
+	if (val & 0x40) != 0 {
+		ret = append(ret, "NOOPEN")
+	}
+	if (val & 0x80) != 0 {
+		ret = append(ret, "ORIGIN")
+	}
+	if (val & 0x100) != 0 {
+		ret = append(ret, "DIRECT")
+	}
+	if (val & 0x200) != 0 {
+		ret = append(ret, "TRANS")
+	}
+	if (val & 0x400) != 0 {
+		ret = append(ret, "INTERPOSE")
+	}
+	if (val & 0x800) != 0 {
+		ret = append(ret, "NODEFLIB")
+	}
+	if (val & 0x1000) != 0 {
+		ret = append(ret, "NODUMP")
+	}
+	if (val & 0x2000) != 0 {
+		ret = append(ret, "CONFLAT")
+	}
+	if (val & 0x4000) != 0 {
+		ret = append(ret, "ENDFILTEE")
+	}
+	if (val & 0x8000) != 0 {
+		ret = append(ret, "DISPRELDNE")
+	}
+	if (val & 0x10000) != 0 {
+		ret = append(ret, "DISPRELPND")
+	}
+	if (val & 0x20000) != 0 {
+		ret = append(ret, "NODIRECT")
+	}
+	if (val & 0x40000) != 0 {
+		ret = append(ret, "IGNMULDEF")
+	}
+	if (val & 0x80000) != 0 {
+		ret = append(ret, "NOKSYMS")
+	}
+	if (val & 0x100000) != 0 {
+		ret = append(ret, "NOHDR")
+	}
+	if (val & 0x200000) != 0 {
+		ret = append(ret, "EDITED")
+	}
+	if (val & 0x400000) != 0 {
+		ret = append(ret, "NORELOC")
+	}
+	if (val & 0x800000) != 0 {
+		ret = append(ret, "SYMINTPOSE")
+	}
+	if (val & 0x1000000) != 0 {
+		ret = append(ret, "GLOBAUDIT")
+	}
+	if (val & 0x2000000) != 0 {
+		ret = append(ret, "SINGLETON")
+	}
+
+	return str.Join(ret, "|")
+}
+
+func makeDynamicInfo(info *DepsInfo) []string {
+	// dynamic attributes
+	var dyns []string
+	for _, v := range info.dyns {
+		switch v.tag {
+		case elf.DT_NEEDED:
+			fallthrough
+		case elf.DT_RPATH:
+			fallthrough
+		case elf.DT_RUNPATH:
+			fallthrough
+		case elf.DT_SONAME:
+			dyns = append(dyns, fmt.Sprintf("  %-16s  %s", v.tag, v.val.(string)))
+		case DT_GNU_HASH:
+			dyns = append(dyns, fmt.Sprintf("  %-16s  %x", "DT_GNU_HASH", v.val))
+		case DT_RELACOUNT:
+			dyns = append(dyns, fmt.Sprintf("  %-16s  %v", "DT_RELACOUNT", v.val))
+		case DT_RELCOUNT:
+			dyns = append(dyns, fmt.Sprintf("  %-16s  %v", "DT_RELCOUNT", v.val))
+		case elf.DT_FLAGS:
+			dyns = append(dyns, fmt.Sprintf("  %-16s  %s", "DT_FLAGS", strFlags(v.val.(uint64))))
+		case DT_FLAGS_1:
+			dyns = append(dyns, fmt.Sprintf("  %-16s  %s", "DT_FLAGS_1", strFlags1(v.val.(uint64))))
+		case DT_VERDEF:
+			dyns = append(dyns, fmt.Sprintf("  %-16s  %x", "DT_VERDEF", v.val))
+		case DT_VERDEFNUM:
+			dyns = append(dyns, fmt.Sprintf("  %-16s  %v", "DT_VERDEFNUM", v.val))
+		case DT_VERNEED:
+			dyns = append(dyns, fmt.Sprintf("  %-16s  %x", "DT_VERNEED", v.val))
+		case DT_VERNEEDNUM:
+			dyns = append(dyns, fmt.Sprintf("  %-16s  %v", "DT_VERNEEDNUM", v.val))
+		default:
+			dyns = append(dyns, fmt.Sprintf("  %-16s  %x", v.tag, v.val))
+		}
+	}
+
+	return dyns
+}
